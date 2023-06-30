8cf55dad0e5f3c5ebb5d8bc14097b659ff15d7de
des-yogi <nuevo-design@mail.ru>
2023-06-30 20:29:44 +0300

    about pages in progress

--- src/blocks/field-text/field-text.js
+++ src/blocks/field-text/field-text.js
@@ -2,3 +2,14 @@
   //if (!autosize()) return;
   autosize(document.querySelectorAll('textarea'));
 })();
+
+(function(){
+  const phoneElems = document.getElementsByClassName('phone-mask');
+  Array.prototype.forEach.call(phoneElems, function (item) {
+    const phoneMask = IMask(
+      item, {
+        mask: '+{38} (\\000) 000 00 00',
+        lazy: false // make placeholder always visible
+    });
+  });
+}());
